c019c2b502047cfe0f7c94ead1e383c18be72ef7
Aftab khan <you@example.com>
2022-01-24 20:47:56 +0530

    My-React

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+export default function Home({ getData }) {
+    return (
+        <section className="home">
+            <div className="content">
+                <p><h1> Click On Get User Button</h1></p>
+            </div>
+        </section>
+    )
+}
